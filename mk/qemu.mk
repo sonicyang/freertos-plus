c98ef88e0b60a40b8b9e19c22d3e1933f8768130
sonicyang <sonic.tw.tp@gmail.com>
2014-10-02 19:59:23 +0800

    add make cdbg for cgdb support

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -18,3 +18,12 @@ qemudbg: $(BUILD_TARGET).bin $(QEMU_STM32)
 	cat $(OUTDIR)/qemu_pid | `xargs kill 2>/dev/null || test true` && \
 	rm -f $(OUTDIR)/qemu_pid
 
+qemucdbg: $(BUILD_TARGET).bin $(QEMU_STM32)
+	$(QEMU_STM32) -M stm32-p103 \
+	    -monitor stdio \
+	    -gdb tcp::3333 -S \
+	    -kernel $(BUILD_TARGET).bin -semihosting 2>&1>/dev/null & \
+	echo $$! > $(OUTDIR)/qemu_pid && \
+	cgdb -d $(CROSS_COMPILE)gdb -x $(TOOLDIR)/gdbscript && \
+	cat $(OUTDIR)/qemu_pid | `xargs kill 2>/dev/null || test true` && \
+	rm -f $(OUTDIR)/qemu_pid
